625a30b9109172237cbdd16c84e9d4c8958b2e77
Mahendra Choudhary <myios7726@gmail.com>
2023-06-19 11:28:24 +0530

    0.1

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -3,3 +3,24 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse('we are at about page')
+
+def contact(request):
+    return HttpResponse('we are at contact page')
+
+def about(request):
+    return HttpResponse('we are at about page')
+
+def tracker(request):
+    return HttpResponse('we are at tracker page')
+
+def search(request):
+    return HttpResponse('we are at search page')
+
+def productView(request):
+    return HttpResponse('we are at product view page')
+
+def checkout(request):
+    return HttpResponse('we are at check out page')
