b4bede58c60baf8816db3d49c995fb7647670f6d
Mahendra Choudhary <myios7726@gmail.com>
2023-06-26 22:32:51 +0530

    0.6.3

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -5,12 +5,23 @@ from math import ceil
 
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//3 + ceil((n/3) - (n//3))
-    params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
-    print(params)
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n//3 + ceil((n/3) - (n//3))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
 
 def about(request):
